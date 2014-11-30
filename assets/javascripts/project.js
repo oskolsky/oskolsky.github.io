11e4f1d7a5e4656aed2c46f09421d7cfa2826cd1
Maxim Oskol'sky <info@oskolsky.com>
2014-11-30 18:58:08 +0300

    Add editor

---
+++ assets/javascripts/project.js
@@ -0,0 +1,91 @@
+//****************************************************************************************************
+//
+// .. READY
+//
+//****************************************************************************************************
+$(function() {
+
+
+
+  //****************************************************************************************************
+  //
+  // .. INITS
+  //
+  //****************************************************************************************************
+  //
+  // .. jQuery UI Sortable init
+  //
+  // $('.js-ui-nav-elements').sortable({
+  //     connectWith: '.ui-section',
+  //     placeholder: 'ui-placeholder',
+
+  //     helper: function (e, li) {
+  //       this.copyHelper = li.clone().insertAfter(li);
+  //       $(this).data('copied', false);
+  //       return li.clone();
+  //     },
+
+  //     stop: function () {
+  //       var copied = $(this).data('copied');
+  //       if (!copied) {
+  //         this.copyHelper.remove();
+  //       }
+  //       this.copyHelper = null;
+  //     }
+  // });
+
+  // $('.js-ui-section').sortable({
+  //   placeholder: 'ui-placeholder',
+
+  //   receive: function (e, ui) {
+  //     ui.sender.data('copied', true);
+  //   }
+  // });
+
+  $('.ui-toolbar div').draggable({
+    helper: function(e) {
+      return $('<div>').addClass('ui-block-drag').html( $(e.target).html() );
+    },
+    connectToSortable: ".ui-content"
+  });
+      
+  $('.ui-content').sortable({
+      placeholder: 'ui-placeholder',
+      update: function (event, ui) {
+        
+        var block = ui.item.data('block');
+        var tpl = _.template($('#block-' + block).html())();
+
+        ui.item.html(tpl).raptor();
+
+      }
+  });
+
+
+
+  //****************************************************************************************************
+  //
+  // .. SCROLL
+  //
+  //****************************************************************************************************
+  $(window).scroll(function() {});
+
+
+
+  //****************************************************************************************************
+  //
+  // .. RESIZE
+  //
+  //****************************************************************************************************
+  $(window).smartresize(function() {});
+  
+});
+
+
+
+//****************************************************************************************************
+//
+// .. LOAD
+//
+//****************************************************************************************************
+$(window).load(function() {});
