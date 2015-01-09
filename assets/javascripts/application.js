6e5a15eb375366f99db58ff8c02a59efb75d7541
unknown <m_oskolskiy@by1-ww-024.corp.wargaming.local>
2015-01-09 11:20:16 +0300

    Deploy

---
+++ assets/javascripts/application.js
@@ -0,0 +1,187 @@
+//****************************************************************************************************
+//
+// .. INIT
+//
+//****************************************************************************************************
+//
+// .. arcticModal
+//
+$.arcticmodal('setDefault', {
+  overlay: {
+    css: {
+      backgroundColor: '#282828',
+      opacity: .95
+    }
+  },
+  openEffect: {
+    speed: 200
+  },
+  closeEffect: {
+    speed: 200
+  },
+  afterClose: function() {
+    window.location.href = window.location.href.split('!')[0];
+  }
+});
+
+//
+// .. Accounting
+//
+accounting.settings = {
+  currency: {
+    decimal: '.',
+    thousand: ' ',
+    precision: 2
+  },
+  number: {
+    decimal : '.',
+    thousand: ' ',
+    precision: 0
+  }
+};
+
+
+
+//****************************************************************************************************
+//
+// .. FUNCTIONS
+//
+//****************************************************************************************************
+//
+// .. Accounting
+//
+function formatNumber() {
+  $('.format-number').each(function() {
+    var
+      number = parseInt($(this).text().replace(new RegExp(' ', 'g'), '')),
+      formatNumber = accounting.formatNumber(number);
+
+    $(this).text(formatNumber);
+  });
+}
+
+function formatMoney() {
+  $('.format-money').each(function() {
+    var c = accounting.settings.currency;
+
+    if ($(this).hasClass('format-money__ru')) {
+      c.format = '%v .–';
+      c.precision = 0;
+    } else if ($(this).hasClass('format-money__us')) {
+      c.symbol = '$';
+      c.format = '%s %v';
+      c.precision = 2;
+    } else if ($(this).hasClass('format-money__eu')) {
+      c.symbol = '€';
+      c.format = '%s %v';
+      c.precision = 2;
+    } else if ($(this).hasClass('format-money__uk')) {
+      c.symbol = '₤';
+      c.format = '%s %v';
+      c.precision = 2;
+    }
+
+    var
+      money = parseFloat($(this).text().replace(new RegExp(' ', 'g'), '')),
+      formatMoney = accounting.formatMoney(money);
+    
+    $(this).text(formatMoney);    
+  });
+}
+
+
+
+//****************************************************************************************************
+//
+// .. EVENTS
+//
+//****************************************************************************************************
+//
+// .. Open dialog
+//
+$(document).on('click touchstart', '[data-dialog="open"]', function() {
+  var url = $(this).data('url');
+
+  $.arcticmodal('close');
+
+  $.arcticmodal({
+    type: 'ajax',
+    url: url
+  });
+  
+  return false;
+});
+
+//
+// .. Close dialog
+//
+$(document).on('click touchstart', '[data-dialog="close"]', function() {
+  $.arcticmodal('close');
+  return false;
+});
+
+
+
+//****************************************************************************************************
+//
+// .. READY
+//
+//****************************************************************************************************
+$(function() {
+
+  //****************************************************************************************************
+  //
+  // .. DOUBLE HOVER
+  //
+  //****************************************************************************************************
+  doubleHover('.double-hover', 'hover');
+
+
+
+  //****************************************************************************************************
+  //
+  // .. FORMS
+  // .. $('#checkbox').customForm() to init single element; $('body').customForm() to init all elements
+  //
+  //****************************************************************************************************
+  $('.js-form').customForm();
+
+
+
+  //****************************************************************************************************
+  //
+  // .. ACCOUNTING
+  //
+  //****************************************************************************************************
+  formatNumber();
+  formatMoney();
+  
+
+
+  //****************************************************************************************************
+  //
+  // .. RESIZE
+  //
+  //****************************************************************************************************
+  $(window).smartresize(function() {
+
+    // $('#header').stickyHeader();
+    $('#footer').stickyFooter();
+
+  });
+  
+});
+
+
+
+//****************************************************************************************************
+//
+// .. LOAD
+//
+//****************************************************************************************************
+$(window).load(function() {
+
+  // $('#header').stickyHeader();
+  $('#footer').stickyFooter();
+
+});
---
+++ assets/javascripts/forms.js
@@ -0,0 +1,274 @@
+(function($) {
+  
+  var
+    updateValidDisabled = function(f, r) {
+      if (r.prop('disabled')) {
+        f.attr('disabled', 'disabled');    
+      } else {
+        f.removeAttr('disabled');
+      }
+      f.attr('data-valid', r.attr('data-valid'));
+    },
+
+    listenFocus = function(f, r) {
+      r.focus(function() {
+        f.addClass('form_el__focus');
+      }).blur(function() {
+        f.removeClass('form_el__focus');
+      });
+    };
+
+  //****************************************************************************************************
+  //
+  // .. ELEMENTS
+  //
+  //****************************************************************************************************
+  //
+  // .. Button
+  //
+  var
+    ButtonHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+
+      listenFocus(this.$el, this.$tag);
+    };
+
+    ButtonHandler.prototype.refresh = function() {
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. Text
+  //
+  var
+    TextHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+
+      listenFocus(this.$el, this.$tag);
+    };
+
+    TextHandler.prototype.refresh = function() {
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. Textarea
+  //
+  var
+    TextareaHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+
+      listenFocus(this.$el, this.$tag);
+    };
+
+    TextareaHandler.prototype.refresh = function() {
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. Checkbox
+  //
+  var
+    CheckboxHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+
+      this.$tag.change(this.refresh.bind(this));
+      listenFocus(this.$el, this.$tag);
+    };
+
+    CheckboxHandler.prototype.refresh = function() {
+      this.$el.attr('data-checked', this.$tag.prop('checked'));
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. Radio
+  //
+  var
+    RadioHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+
+      this.$tag.change(this.change.bind(this));
+      listenFocus(this.$el, this.$tag);
+    };
+
+    RadioHandler.prototype.change = function() {
+      var
+        form = this.$tag.closest('form'),
+        name = this.$tag.attr('name'),
+        parent, toRefresh;
+
+      if (form.length > 0) {
+        parent = form[0];
+      } else {
+        parent = 'body';
+      }
+
+      if (name) {
+        toRefresh = $(parent).find('[name=' + name + ']:radio');
+      } else {
+        toRefresh = $(parent).find(':radio');
+      }
+
+      toRefresh.customForm('refresh');
+    };
+
+    RadioHandler.prototype.refresh = function() {
+      this.$el.attr('data-checked', this.$tag.prop('checked'));
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. Select
+  //
+  var
+    SelectHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+      this.$inner = this.$el.find('.js-form_el_inner');
+
+      this.$tag.change(this.refresh.bind(this));
+      listenFocus(this.$el, this.$tag);
+    };
+
+    SelectHandler.prototype.refresh = function() {
+      var
+        text = this.$tag.children('option:selected').text();
+
+      this.$inner.text(text);
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+  //
+  // .. File
+  //
+  var
+    FileHandler = function(tag) {
+      this.$tag = $(tag);
+      this.$el = this.$tag.closest('.js-form_el');
+      this.$inner = this.$el.find('.js-form_el_inner');
+      this.placeholder = this.$el.attr('data-placeholder');
+
+      var
+        real = this.$tag,
+        locked = false;
+
+      this.$el.click(function() {
+        if (locked) {
+          return;
+        }
+
+        if (!real.prop('disabled')) {
+          locked = true;
+          real.trigger('click');
+          locked = false;
+        }
+      });
+      this.$tag.change(this.refresh.bind(this));
+      listenFocus(this.$el, this.$tag);
+    };
+
+    FileHandler.prototype.refresh = function() {
+      var
+        filename = this.$tag.val().split(/[\\\/]/).pop();
+
+      if (!filename) {
+        filename = this.placeholder;
+      }
+
+      this.$inner.text(filename);
+      updateValidDisabled(this.$el, this.$tag);
+    };
+
+
+  //****************************************************************************************************
+  //
+  // .. BASE
+  //
+  //****************************************************************************************************
+  var
+    customizers = {
+      button: ButtonHandler,
+      text: TextHandler,
+      textarea: TextareaHandler,
+      checkbox: CheckboxHandler,
+      radio: RadioHandler,
+      select: SelectHandler,
+      file: FileHandler
+    },
+
+    handler = function(el, type) {
+      var
+        dataName = 'customForm',
+        data = $(el).data(dataName);
+      if (!data) {
+        data = new customizers[type](el);
+        $(el).data(dataName, data);
+      }
+      return data;
+    };
+
+  var
+    refreshElements = function(selector, type) {
+      this.find(selector).add(this.filter(selector)).each(function() {
+        handler(this, type).refresh();
+      });
+    },
+    methods = {
+      init: function() {
+        this.find('[type="reset"]').click(function() {
+          if (this.form) {
+            this.form.reset();
+            $(this.form).customForm('reset');
+          }
+          return false;
+        });
+        return this.customForm('refresh');
+      },
+
+      refresh: function() {
+        return this.each(function() {
+          var 
+            r = refreshElements.bind($(this));
+
+          r('[type="button"]', 'button');
+          r('[type="reset"]', 'button');
+          r('[type="submit"]', 'button');
+          
+          r('input[type="text"]', 'text');
+          r('input[type="password"]', 'text');
+          r('input[type="email"]', 'text');
+          r('input[type="tel"]', 'text');
+          
+          r('input[type="checkbox"]', 'checkbox');
+          r('input[type="radio"]', 'radio');
+          r('input[type="file"]', 'file');
+          
+          r('select', 'select');
+          r('textarea', 'textarea');
+        });
+      },
+
+      reset: function() {
+        return this.each(function() {
+          $(this).find('[data-valid]').removeAttr('data-valid');
+        }).customForm('refresh');
+      }
+    };
+
+  $.fn.customForm = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('jQuery.customForm: unsupported method - ' +  method);
+    }
+  };
+
+})(jQuery);
