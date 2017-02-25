1112e325a653160875caa935e8331306c4c708ae
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2017-02-25 16:25:24 +0300

    .

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,5 +1,27 @@
 $(function() {
 
+    $('.js-owl-carousel-specifications').owlCarousel({
+        items: 1,
+        center: false,
+        loop: false,
+        dots: true,
+        margin: 40,
+        responsive: {
+            0: {
+                items: 1,
+                mouseDrag: true,
+                touchDrag: true,
+                pullDrag: true
+            },
+            639: {
+                items: 4,
+                mouseDrag: false,
+                touchDrag: false,
+                pullDrag: false
+            }
+        }
+    })
+
     $('.js-owl-carousel').owlCarousel({
         center: true,
         items: 2,
