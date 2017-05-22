f44142fd6fe43482c80a39ba8c49cad66937d3f9
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2017-05-22 05:05:49 +0300

    .

--- jura/assets/javascripts/script.js
+++ jura/assets/javascripts/script.js
@@ -4,6 +4,12 @@ $(function() {
 
     // Mask input
     $('#js-input-phone').mask('+7 (999) 999-9999');
+
+    // Scroll to bottom
+    $('#scroll-to-bottom').click(function() {
+        $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+        return false;
+    });
 });
 
 $(window).resize(function() {
