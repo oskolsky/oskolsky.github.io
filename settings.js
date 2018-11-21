dc921b851aa959d5cc06cec7a597a5e29a8a1aca
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2018-11-21 18:32:08 +0300

    .

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 /* eslint-disable no-unused-vars */
-var API_HOST = 'http://www.mocky.io/v2/5bf28faa2f00005400cfa14c'
+var API_HOST = 'http://www.mocky.io/v2/5bf579bc3000007d007bbea7'
 var SET_INTERVAL = 300000 // 5 min
